cb26e38e50d375ed26ad04a9c9accbe464d7f59a
Jingly <1669872242@qq.com>
2016-08-22 16:24:26 +0800

    add

---
+++ GarfieldProjects/utils/src/main/java/com/example/utils/GarfieldTestActivity.java
@@ -0,0 +1,13 @@
+package com.example.utils;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GarfieldTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
